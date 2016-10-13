5c241a26bd0a2706d4bc3eb41208f736de11116c
Peter Lane <lane@uchicago.edu>
2016-10-13 14:01:20 -0500

    removed lingering NC RabbitMQ setup commands

--- scripts/setup-rabbitmq.sh
+++ scripts/setup-rabbitmq.sh
@@ -17,14 +17,3 @@ echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sour
 wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
 apt-get update
 apt-get install -y rabbitmq-server
-
-# Ensure that RabbitMQ server is running.
-service rabbitmq-server start
-
-# Enable RabbitMQ plugins.
-rabbitmq-plugins enable rabbitmq_management rabbitmq_shovel rabbitmq_shovel_management
-
-# Configure RabbitMQ shovels.
-#rabbitmqctl set_parameter shovel data-shovel "{\"src-uri\": \"amqp://localhost\", \"src-queue\": \"data\", \"dest-uri\": \"amqps://node:waggle@beehive1.mcs.anl.gov:23181?cacertfile=/usr/lib/waggle/SSL/waggleca/cacert.pem&certfile=/usr/lib/waggle/SSL/node/cert.pem&keyfile=/usr/lib/waggle/SSL/node/key.pem&verify=verify_peer\", \"dest-exchange\": \"data-pipeline-in\", \"ack-mode\": \"on-confirm\", \"reconnect-delay\": 60, \"publish-properties\": {\"delivery_mode\": 2, \"reply_to\": \"$WAGGLE_ID\"}}"
-#rabbitmqctl set_parameter shovel logs-shovel "{\"src-uri\": \"amqp://localhost\", \"src-queue\": \"logs\", \"dest-uri\": \"amqps://node:waggle@beehive1.mcs.anl.gov:23181?cacertfile=/usr/lib/waggle/SSL/waggleca/cacert.pem&certfile=/usr/lib/waggle/SSL/node/cert.pem&keyfile=/usr/lib/waggle/SSL/node/key.pem&verify=verify_peer\", \"dest-exchange\": \"logs\", \"ack-mode\": \"on-confirm\", \"reconnect-delay\": 60, \"publish-properties\": {\"delivery_mode\": 2, \"reply_to\": \"$WAGGLE_ID\"}}"
-#rabbitmqctl set_parameter shovel image-shovel "{\"src-uri\": \"amqp://localhost\", \"src-queue\": \"images\", \"dest-uri\": \"amqps://node:waggle@beehive1.mcs.anl.gov:23181?cacertfile=/usr/lib/waggle/SSL/waggleca/cacert.pem&certfile=/usr/lib/waggle/SSL/node/cert.pem&keyfile=/usr/lib/waggle/SSL/node/key.pem&verify=verify_peer\", \"dest-exchange\": \"images\", \"ack-mode\": \"on-confirm\", \"reconnect-delay\": 60, \"publish-properties\": {\"delivery_mode\": 2, \"reply_to\": \"$WAGGLE_ID\"}}"
