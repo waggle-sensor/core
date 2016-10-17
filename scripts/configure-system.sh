d20cdf8238597953242438f2b22cc2c2ad96acce
Peter Lane <lane@uchicago.edu>
2016-10-17 10:45:35 -0500

    disable NTP time sync

--- scripts/configure-system.sh
+++ scripts/configure-system.sh
@@ -45,7 +45,8 @@ cp ${script_dir}/aafirstboot /
 sed -i -e 's:^TimeoutStartSec=5min:TimeoutStartSec=5sec:' /lib/systemd/system/networking.service
 sed -i -e 's:^TimeoutStartSec=5min:TimeoutStartSec=5sec:' /lib/systemd/system/ifup@.service
 systemctl disable apt-daily.timer
-#systemctl disable unattended-upgrades.service
+systemctl disable time-sync.target
+systemctl disable systemd-timesyncd
 
 # Restrict SSH connections to local port bindings
 sed -i 's/^#ListenAddress ::$/ListenAddress 127.0.0.1/' /etc/ssh/sshd_config
