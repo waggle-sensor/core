a36838edde0710eb906d22a20e03487e2f6bb368
Peter Lane <lane@uchicago.edu>
2016-10-18 12:42:31 -0500

    shell magic

--- scripts/configure-static-iface.sh
+++ scripts/configure-static-iface.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
-/usr/lib/waggle/core/scripts/detect_mac_address.sh
+. /usr/lib/waggle/core/scripts/detect_mac_address.sh
 sed -i -e "s/%MAC_STRING%/$MAC_STRING/" /etc/network/interfaces
