fd8d83faceb5c0bc59f96d4a83d8047e8d788408
Peter Lane <lane@uchicago.edu>
2016-10-18 17:01:36 -0500

    reverted mac address detection to searching for 00:1e:06 prefix since ifindex value can change between boots and is not reliable

--- scripts/detect_mac_address.sh
+++ scripts/detect_mac_address.sh
@@ -2,26 +2,31 @@
 
 # returns
 export MAC_ADDRESS=""
-export MAC_STRING=""
-export NODE_ID=""
+export MAC_STRING="" 
+export NODE_ID="" 
 
 set +e
+while [ "${MAC_ADDRESS}x" == "x" ] ; do
+  export MAC_ADDRESS=$(ip link | grep '00:1e:06' | awk '{print $2}')
+  export MAC_STRING=$(echo $MAC_ADDRESS | sed 's/://g')
 
-ifaces=$(ls -d /sys/class/net/enx* /sys/class/net/eth*)
-for iface in $ifaces; do
-  export $(udevadm info --path=$iface | grep IFINDEX | awk '{print $2}')
-  if [ $IFINDEX == 2 ]; then
-    export $(udevadm info -p $iface | grep "E: ID_NET_NAME_MAC" | awk '{print $2}')
-    export $(udevadm info -p $iface | grep "E: INTERFACE" | awk '{print $2}')
-    export MAC_STRING=$(echo "$ID_NET_NAME_MAC" | sed 's/enx//')
-    export MAC_ADDRESS=$(ip link show $INTERFACE| grep -A 1 '^2:' | grep 'link/ether' | awk '{print $2}')
-    export NODE_ID="0000${MAC_STRING}"
+  if [ "${MAC_ADDRESS}x" == "x" ] ; then
+    echo "MAC_ADDRESS not found, retrying..."
+    sleep 3
   fi
+  
 done
-
 set -e
 
 
 echo "MAC_ADDRESS=${MAC_ADDRESS}"
 echo "MAC_STRING=${MAC_STRING}"
+
+if [ ! ${#MAC_STRING} -ge 12 ]; then
+  echo "error: could not extract MAC address"
+  exit 1
+else
+  NODE_ID="0000${MAC_STRING}"
+fi
+
 echo "NODE_ID=${NODE_ID}"
