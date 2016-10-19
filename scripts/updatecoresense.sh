34ca56b07b5205b26ca89cd45585620d30de00fe
Raj Sankaran <rajesh@anl.gov>
2016-10-19 12:11:16 -0500

    Utilities to flash coresense boards.

---
+++ scripts/updatecoresense.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#download the file 
+rm /tmp/coresense_fw.ino.bin
+wget http://www.mcs.anl.gov/research/projects/waggle/downloads/coresense/firmware.ino.bin -O /tmp/coresense_fw.ino.bin
+
+# Ask wagman to powerdown the coresense board
+wagman-client stop 2 0
+sleep 2
+#Ask wagman to powerup the coresense board
+wagman-client start 2
+
+#then call coresense flash
+/usr/lib/waggle/core/scripts/coresenseflash /tmp/coresense_fw.ino.bin
