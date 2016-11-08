5753b457ed9e085ab71f5440e005b9e2bf997be7
Peter Lane <lane@uchicago.edu>
2016-11-08 11:14:23 -0600

    moved the pip update before any pip installs

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,10 +2,10 @@
 
 apt-get install -y htop iotop iftop bwm-ng screen git python-dev python-pip python3-dev python3-pip tree psmisc dosfstools parted bash-completion fswebcam v4l-utils network-manager usbutils nano stress-ng
 
+pip3 install -U pip
 
 # python2 dependencies
 pip install tabulate
 
 # python3 dependencies
-pip3 install -U pip
 pip3 install tabulate
