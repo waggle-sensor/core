71b2f2be5abd18d0fc3a34f6e4a8855981e5e091
Peter Lane <lane@uchicago.edu>
2016-11-08 10:05:24 -0600

    Added stress-ng to the dependencies

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -y htop iotop iftop bwm-ng screen git python-dev python-pip python3-dev python3-pip tree psmisc dosfstools parted bash-completion fswebcam v4l-utils network-manager usbutils nano
+apt-get install -y htop iotop iftop bwm-ng screen git python-dev python-pip python3-dev python3-pip tree psmisc dosfstools parted bash-completion fswebcam v4l-utils network-manager usbutils nano stress-ng
 
 
 # python2 dependencies
