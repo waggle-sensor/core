93162690897ddba853b2f7709f9cd08df9b7f212
Raj Sankaran <rajesh@anl.gov>
2016-12-07 13:30:27 -0600

    Progress on Wagman update script

--- scripts/updatewagman.sh
+++ scripts/updatewagman.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #download the file 
-rm /tmp/wagman_fw.ino.bin
-wget http://www.mcs.anl.gov/research/projects/waggle/downloads/wagman/firmware.ino.bin -O /tmp/wagman_fw.ino.bin
+rm /tmp/wagman_fw.ino.hex
+wget http://www.mcs.anl.gov/research/projects/waggle/downloads/wagman/firmware.ino.hex -O /tmp/wagman_fw.ino.hex
 
 #then call coresense flash
-./wagmanflash /tmp/wagman_fw.ino.bin
+./wagmanflash /tmp/wagman_fw.ino.hex
