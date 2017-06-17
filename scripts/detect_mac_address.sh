42ab8238ac886e43d8f512d6975eeb224843db94
Peter Lane <muffinspawn@gmail.com>
2017-06-17 13:26:30 -0500

    moved node ID determination out of MAC address detection script into existing node ID creation script where it belongs

--- scripts/detect_mac_address.sh
+++ scripts/detect_mac_address.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-if [ "x${NODE_ID_PREFIX}" != "x" ]; then
-  declare -r node_id_prefix=${NODE_ID_PREFIX}
-else
-  declare -r node_id_prefix="0000"
-fi
-
 # returns
 mac_address=""
 mac_string=""
-node_id=""
 
 set +e
-mac_address=$(ip link | grep -e '00:1e:06' -e '02:' | awk '{print $2}')
+# All Odroid MAC addresses (so far) have the organizational prefix 00:1e:06
+mac_address=$(ip link | grep -e '00:1e:06' | awk '{print $2}')
+if [ "x$mac_address" == "x" ]; then
+  # Locally Administered Mac Addresses
+  # TODO: look for any addresses in the following form:
+  # x2-xx-xx-xx-xx-xx
+  # x6-xx-xx-xx-xx-xx
+  # xA-xx-xx-xx-xx-xx
+  # xE-xx-xx-xx-xx-xx
+  mac_address=$(ip link | grep -e '02:' | awk '{print $2}')
+fi
 mac_string=$(echo $mac_address | sed 's/://g')
 
 if [ "${mac_address}x" == "x" ] ; then
@@ -21,18 +24,13 @@ if [ "${mac_address}x" == "x" ] ; then
 fi
 set -e
 
-
-if [ ! ${#mac_string} -eq 17 ]; then
-  node_id="${node_id_prefix}${mac_string}"
-else
+if [ ${#mac_string} -ne 12 ]; then
   echo "error: bad MAC address '${mac_address}'"
   exit 2
 fi
 
 export MAC_ADDRESS=$mac_address
 export MAC_STRING=$mac_string
-export NODE_ID=$node_id
 
 echo "MAC_ADDRESS=${MAC_ADDRESS}"
 echo "MAC_STRING=${MAC_STRING}"
-echo "NODE_ID=${NODE_ID}"
