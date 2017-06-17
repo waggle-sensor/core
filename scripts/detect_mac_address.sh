7dc97dce979ae39bc56fb1e1deb2cec8ae0de86d
Peter Lane <muffinspawn@gmail.com>
2017-06-17 12:10:14 -0500

    minimally modified MAC address/node ID generation to accomodate docker virtual nodes

--- scripts/detect_mac_address.sh
+++ scripts/detect_mac_address.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 
+if [ "x${NODE_ID_PREFIX}" != "x" ]; then
+  declare -r node_id_prefix=${NODE_ID_PREFIX}
+else
+  declare -r node_id_prefix="0000"
+fi
+
 # returns
-export MAC_ADDRESS=""
-export MAC_STRING="" 
-export NODE_ID="" 
+mac_address=""
+mac_string=""
+node_id=""
 
 set +e
-while [ "${MAC_ADDRESS}x" == "x" ] ; do
-  export MAC_ADDRESS=$(ip link | grep '00:1e:06' | awk '{print $2}')
-  export MAC_STRING=$(echo $MAC_ADDRESS | sed 's/://g')
+mac_address=$(ip link | grep -e '00:1e:06' -e '02:' | awk '{print $2}')
+mac_string=$(echo $mac_address | sed 's/://g')
 
-  if [ "${MAC_ADDRESS}x" == "x" ] ; then
-    echo "MAC_ADDRESS not found, retrying..."
-    sleep 3
-  fi
-  
-done
+if [ "${mac_address}x" == "x" ] ; then
+  echo "error: MAC address not found."
+  exit 1
+fi
 set -e
 
 
-echo "MAC_ADDRESS=${MAC_ADDRESS}"
-echo "MAC_STRING=${MAC_STRING}"
-
-if [ ! ${#MAC_STRING} -ge 12 ]; then
-  echo "error: could not extract MAC address"
-  exit 1
+if [ ! ${#mac_string} -eq 17 ]; then
+  node_id="${node_id_prefix}${mac_string}"
 else
-  NODE_ID="0000${MAC_STRING}"
+  echo "error: bad MAC address '${mac_address}'"
+  exit 2
 fi
 
+export MAC_ADDRESS=$mac_address
+export MAC_STRING=$mac_string
+export NODE_ID=$node_id
+
+echo "MAC_ADDRESS=${MAC_ADDRESS}"
+echo "MAC_STRING=${MAC_STRING}"
 echo "NODE_ID=${NODE_ID}"
