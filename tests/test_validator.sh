acbef72c7658a4732b34a8d912deb762d4d07a24
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-10-31 10:09:52 -0500

    added test case for v1 and v2 status output

--- tests/test_validator.sh
+++ tests/test_validator.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-echo "validate status v2"
+echo "test - validate status v1"
+../scripts/status-validator < status_example_v1.json
+
+echo "test - validate status v2"
 ../scripts/status-validator < status_example_v2.json
