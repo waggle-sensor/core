4e9203785d9baca0503098ab2a73b32a90d814bd
Vince Forgione <vforgione@uchicago.edu>
2019-04-08 12:37:35 -0500

    Added license notification headers
    I ran the header injection script and one manual addition.

--- scripts/configure-rabbitmq.sh
+++ scripts/configure-rabbitmq.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# ANL:waggle-license
+#  This file is part of the Waggle Platform.  Please see the file
+#  LICENSE.waggle.txt for the legal details of the copyright and software
+#  license.  For more details on the Waggle project, visit:
+#           http://www.wa8.gl
+# ANL:waggle-license
 
 echo -n "Checking .erlang.cookie..."
 cookie_length_check_result=$(cat /var/lib/rabbitmq/.erlang.cookie | wc -c)
@@ -9,4 +15,4 @@ else
   rm -f /var/lib/rabbitmq/.erlang.cookie
 fi
 
-echo "Checking done"
+echo "Checking done
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# ANL:waggle-license
+#  This file is part of the Waggle Platform.  Please see the file
+#  LICENSE.waggle.txt for the legal details of the copyright and software
+#  license.  For more details on the Waggle project, visit:
+#           http://www.wa8.gl
+# ANL:waggle-license
 
 apt-get install -y htop iotop iftop bwm-ng screen git python-dev python-pip python3-dev python3-pip tree psmisc dosfstools parted bash-completion fswebcam v4l-utils network-manager usbutils nano stress-ng
 
