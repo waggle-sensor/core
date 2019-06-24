d5c1c25a146ed23c84345b943f7663e2880d67b4
Yongho Kim <ironyonghokim@gmail.com>
2019-06-24 13:57:05 -0500

    Added TX2 case for detecting disk devices

--- scripts/detect_disk_devices.sh
+++ scripts/detect_disk_devices.sh
@@ -6,7 +6,7 @@
 #           http://www.wa8.gl
 # ANL:waggle-license
 
-export CURRENT_DISK_DEVICE=$(mount | grep "on / " | cut -f 1 -d ' ' | grep -o "/dev/mmcblk[0-1]")
+export CURRENT_DISK_DEVICE=$(mount | grep "on / " | cut -f 1 -d ' ' | grep -o "/dev/mmcblk[0-2]")
 export OTHER_DISK_DEVICE=""
 
 if [ "${CURRENT_DISK_DEVICE}x" == "x" ] ; then
@@ -24,6 +24,10 @@ fi
 
 if [ "${CURRENT_DISK_DEVICE}x" == "/dev/mmcblk1x" ] ; then
   export CURRENT_DISK_DEVICE_NAME="mmcblk1"
+  export OTHER_DISK_DEVICE="/dev/mmcblk0"
+  export OTHER_DISK_DEVICE_NAME="mmcblk0"
+elif [ "${CURRENT_DISK_DEVICE}x" == "/dev/mmcblk2x" ] ; then
+  export CURRENT_DISK_DEVICE_NAME="mmcblk2"
   export OTHER_DISK_DEVICE="/dev/mmcblk0"
   export OTHER_DISK_DEVICE_NAME="mmcblk0"
 fi
